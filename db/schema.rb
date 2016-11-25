07499553625b633929c6aadb9a42d4a376d3f48b
Egor Dubrovin <dubrovin.en@gmail.com>
2016-11-25 16:26:01 +0300

    added ArticleAddColumns migration content

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125124100) do
+ActiveRecord::Schema.define(version: 20161125131031) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
+    t.string   "author_id"
   end
 
   create_table "users", force: :cascade do |t|
