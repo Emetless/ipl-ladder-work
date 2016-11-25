682d7488321cadfb37a76e8d69c5012a8bac43b5
Egor Dubrovin <dubrovin.en@gmail.com>
2016-11-25 16:53:11 +0300

    added Articles controller code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,18 @@
 class ArticlesController < ApplicationController
   # before_filter :authenticate_user!
   def index
+    @articles = Article.all
   end
 
   def edit
+  end
+
+  def new
+    article = Article.new do |article|
+      article.author_id = 1
+      article.content = params[:content]
+    end
+    article.save
+    redirect_to articles_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'articles/edit'
 
+  post 'new_article', to: 'articles#new'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
