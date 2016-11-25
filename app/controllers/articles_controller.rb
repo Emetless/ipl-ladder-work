1b06804555fbff4c2da917361e722323fe35bd05
Egor Dubrovin <dubrovin.en@gmail.com>
2016-11-25 21:59:22 +0300

    Edit and New update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,34 @@
 class ArticlesController < ApplicationController
-  # before_filter :authenticate_user!
   def index
-    @articles = Article.all
+    @articles = Article.where author_id: current_user.id
+    @edit = true
   end
 
-  def edit
+  def user
+    @articles = Article.where author_id: params[:id]
+    @edit = false
+    render :index
   end
 
   def new
+  end
+
+  def create
     article = Article.new do |article|
-      article.author_id = 1
+      article.author_id = current_user.id
       article.content = params[:content]
     end
     article.save
+    redirect_to articles_index_path
+  end
+
+  def edit
+    @article = Article.find_by_id params[:article_id]
+  end
+
+  def update
+    @article = Article.find_by_id params[:article_id]
+    @article.update content: params[:content]
     redirect_to articles_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   get 'articles/index'
 
+  get 'articles/user'
+
+  get 'articles/new'
+
+  post 'articles/create'
+
   get 'articles/edit'
 
-  post 'new_article', to: 'articles#new'
+  post 'articles/update'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
