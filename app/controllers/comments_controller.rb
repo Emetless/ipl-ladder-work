ae12cd01742a4a78ab4b43f2a62aa4b3cff5130a
Egor Dubrovin <dubrovin.en@gmail.com>
2016-12-02 16:59:39 +0300

    added comment fields

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new comment_params
+  end
+
+  private
+
+  def comment_params
+    params.require :comment
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'comments/create'
+  post 'comments/create'
 
   get 'users', to: 'articles#users'
 
