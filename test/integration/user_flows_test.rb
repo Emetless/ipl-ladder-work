8ce6efea3f72887ca2983a475b02340b1034ef93
Egor Dubrovin <dubrovin.en@gmail.com>
2016-12-04 23:22:53 +0300

    Added fail integration tests

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -45,4 +45,49 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
 
     assert Comment.where(author_id: @user_for_tests.id, content: 'Test Comment', article_id: articles(:test_article).id)
   end
+
+  test 'fail sign up' do
+    post user_registration_path, params:
+      { 'user[email]' => 'email@example.com',
+        'user[password]' => 'password',
+        'user[password_confirmation]' => 'password1' }
+
+    assert_response :success # Controller didn't redirect to root_path
+  end
+
+  test 'sign in and fail to write an article' do
+    post user_session_path, params:
+      { 'user[email]' => @user_for_tests.email,
+        'user[password]' => 'password' }
+
+    assert_redirected_to root_path
+
+    post articles_create_path, params: { 'article[content]' => '' }
+    assert_redirected_to root_path
+
+    assert !Article.exists?(content: '')
+  end
+
+  test 'sign in and fail to write a comment' do
+    post user_session_path, params:
+      { 'user[email]' => @user_for_tests.email,
+        'user[password]' => 'password' }
+
+    assert_redirected_to root_path
+    follow_redirect!
+
+    assert_select '.card>p', 'What about some tests?'
+
+    post comments_create_path(format: :json), params:
+      { 'comment[content]' => '',
+        'comment[article_id]' => articles(:test_article).id }
+
+    assert !Comment.exists?(author_id: @user_for_tests.id, content: '', article_id: articles(:test_article).id)
+
+    post comments_create_path(format: :json), params:
+      { 'comment[content]' => 'Test Comment',
+        'comment[article_id]' => 0 }
+
+    assert !Comment.exists?(author_id: @user_for_tests.id, content: 'Test Comment', article_id: 0)
+  end
 end
